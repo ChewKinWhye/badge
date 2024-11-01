d3735661bd42b79f6e4d57d0e13249d2f7e5d6e7
ChewKinWhye <e0200920@u.nus.edu>
2024-11-01 10:39:40 +0800

    Added celeba dataset

--- utils/celeba.py
+++ utils/celeba.py
@@ -9,7 +9,7 @@ import pandas as pd
 
 
 class CELEBA(Dataset):
-    def __init__(self, x, y, p, isTrain, target_resolution=(512, 512)):
+    def __init__(self, x, y, p, isTrain, target_resolution=(224, 224)):
         self.x = x
         self.y_array = y
         self.p_array = p
@@ -48,37 +48,49 @@ class CELEBA(Dataset):
         return img
 
 
-def get_celeba(data_dir, spurious_strength, seed):
-    VAL_SIZE = 1000
-    TEST_SIZE = 2000
-
-    save_dir = os.path.join(data_dir, f"celeba-{spurious_strength}-{seed}.pkl")
+def get_celeba(data_dir, seed):
+    # Random seed does not affect data generation
+    save_dir = os.path.join(data_dir, f"celeba.pkl")
     if os.path.exists(save_dir):
         print("Loading Dataset")
         with open(save_dir, 'rb') as f:
             X_train, Y_train, P_train, X_val, Y_val, P_val, X_test, Y_test, P_test = pickle.load(f)
         return X_train, Y_train, P_train, X_val, Y_val, P_val, X_test, Y_test, P_test
-
     print("Generating Dataset")
+
+    with open(os.path.join(data_dir, "celeba/list_eval_partition.txt"), "r") as f:
+        splits = f.readlines()
+    with open(os.path.join(data_dir, "celeba/list_attr_celeba.txt"), "r") as f:
+        attrs = f.readlines()[2:]
+    f = open(os.path.join(data_dir, "metadata_celeba.csv"), "w")
+    f.write("id,filename,split,y,a\n")
+    for i, (split, attr) in enumerate(zip(splits, attrs)):
+        fi, si = split.strip().split()
+        ai = attr.strip().split()[1:]
+        yi = 1 if ai[9] == "1" else 0
+        gi = 1 if ai[20] == "1" else 0
+        f.write("{},{},{},{},{}\n".format(i + 1, fi, si, yi, gi))
+    f.close()
     root = os.path.join(data_dir, "celeba/img_align_celeba/")
     metadata = os.path.join(data_dir, "metadata_celeba.csv")
     df = pd.read_csv(metadata)
-    # Train Dataset
-    df_train = df[df["split"] == 0]
-    X_train = df_train["filename"].astype(str).map(lambda x: os.path.join(root, x)).tolist()
-    Y_train = np.array(df["y"])
-    P_train = np.array(df["a"])
-    # Val Dataset
-    df_val = df[df["split"] == 1]
-    X_val = df_val["filename"].astype(str).map(lambda x: os.path.join(root, x)).tolist()
-    Y_val = np.array(df_val["y"])
-    P_val = np.array(df_val["a"])
-    # Test Dataset
-    df_test = df[df["split"] == 2]
-    X_test = df_test["filename"].astype(str).map(lambda x: os.path.join(root, x)).tolist()
-    Y_test = np.array(df_test["y"])
-    P_test = np.array(df_test["a"])
+    # Train dataset
+    df_subset = df[df["split"] == 0]
+    X_train = df_subset["filename"].astype(str).map(lambda x: os.path.join(data_dir, root, x)).tolist()
+    Y_train = np.array(df_subset["y"].tolist())
+    P_train = np.array(df_subset["a"].tolist())
+    # Validation dataset
+    df_subset = df[df["split"] == 1]
+    X_val = df_subset["filename"].astype(str).map(lambda x: os.path.join(data_dir, root, x)).tolist()
+    Y_val = np.array(df_subset["y"].tolist())
+    P_val = np.array(df_subset["a"].tolist())
+    # Test dataset
+    df_subset = df[df["split"] == 2]
+    X_test = df_subset["filename"].astype(str).map(lambda x: os.path.join(data_dir, root, x)).tolist()
+    Y_test = np.array(df_subset["y"].tolist())
+    P_test = np.array(df_subset["a"].tolist())
 
     with open(save_dir, 'wb') as f:
         pickle.dump((X_train, Y_train, P_train, X_val, Y_val, P_val, X_test, Y_test, P_test), f)
+
     return X_train, Y_train, P_train, X_val, Y_val, P_val, X_test, Y_test, P_test
