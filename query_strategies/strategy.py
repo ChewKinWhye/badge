fd409241f543749439164f99bac9427bf2cf7fa1
ChewKinWhye <e0200920@u.nus.edu>
2025-06-22 15:39:06 +0800

    Resample using weights

--- query_strategies/strategy.py
+++ query_strategies/strategy.py
@@ -1,4 +1,4 @@
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, WeightedRandomSampler
 from torch.autograd import Variable
 from utils.utils import AverageMeter, get_output, AverageGroupMeter, infinite_dataloader
 import time
@@ -256,7 +256,8 @@ class Strategy:
                 weight_optimizer.step()
                 with torch.no_grad():
                     dataset_weights.clamp_(min=0.1)
-
+            with torch.no_grad():
+                dataset_weights.data = dataset_weights.data / dataset_weights.data.mean()
             # Print learned dataset weights
             print(f"Epoch {epoch}")
             print(f"Average Minority Weight: {torch.mean(dataset_weights[self.Y[idxs_metatrain]!=self.P[idxs_metatrain]])}")
@@ -268,7 +269,16 @@ class Strategy:
         self.clf = self.clf.cuda()
         optimizer = optim.Adam(self.clf.parameters(), lr=self.args.lr, weight_decay=self.args.weight_decay)
         criterion = torch.nn.CrossEntropyLoss()
+        sample_weights = torch.tensor([dataset_weights.detach().cpu().tolist()] + [1]*len(loader_metatest.dataset))
+        sampler = WeightedRandomSampler(weights=sample_weights,
+                                        num_samples=len(sample_weights),  # or any number of samples per epoch
+                                        replacement=True)
 
+        loader_tr = DataLoader(self.handler([self.X[i] for i in idxs_metatrain]+[self.X[i] for i in idxs_metatest],
+                                            torch.Tensor(np.concatenate([self.Y[idxs_metatrain], self.Y[idxs_metatest]], axis=0)).long(),
+                                            torch.Tensor(np.concatenate([self.P[idxs_metatrain], self.P[idxs_metatest]], axis=0)).long(),
+                                            isTrain=True, target_resolution=self.target_resolution),
+                               batch_size=self.args.batch_size, sampler=sampler)
         # --- Train Start ---
         best_val_min_acc, best_epoch = -1, None
 
@@ -277,16 +287,12 @@ class Strategy:
             # Track metrics
             ce_loss_meter, train_group_acc = AverageMeter(), AverageGroupMeter(self.num_classes, self.num_attributes)
             start = time.time()
-            for batch in tqdm.tqdm(loader_metatrain, disable=True):
+            for batch in tqdm.tqdm(loader_tr, disable=True):
                 x, y, p, idxs = batch
                 x, y, p, idxs = x.cuda(), y.cuda(), p.cuda(), idxs.cuda()
-                sample_weights = dataset_weights[idxs]
-                sample_weights = sample_weights / torch.sum(sample_weights)
                 optimizer.zero_grad()
                 logits = self.clf(x)
-                sample_loss = torch.nn.CrossEntropyLoss(reduction='none')(logits, y)
-                loss = torch.sum(sample_loss * sample_weights.detach())
-                loss += criterion(self.clf(x_meta), y_meta)
+                loss = criterion(logits, y)
                 loss.backward()
                 torch.nn.utils.clip_grad_norm_(self.clf.parameters(), 1.0)
                 optimizer.step()
